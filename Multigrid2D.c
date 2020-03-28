2946717823e6d9dab2732302c18209fbe9640486
Abdul Hannan Faruqi <abdul.hannan@zhcet.ac.in>
2020-03-28 11:48:30 +0530

    Update and rename Multigrid Code.c to Multigrid2D.c

--- Multigrid2D.c
+++ Multigrid2D.c
@@ -24,7 +24,7 @@ if(fp == NULL){
     return 1;
 }
 
-fp2 = fopen("Plot.txt", "w");   //For plotting results in Tecplot
+fp2 = fopen("Plot.dat", "w");   //For plotting results in Tecplot
 if(fp2 == NULL){
     printf("Couldn't open file\n");
     return 1;
@@ -48,14 +48,14 @@ x: printf("\n\nChoose an AMG Scheme\nEnter a character: [V]\t[W]\n");
 scanf("%c", &sch);
 if (sch == 'V')
 {
-fprintf(fp, "V,");
+    fprintf(fp, "V,");
 }
 if (sch=='W')
 {
-fprintf(fp, "W,");
-printf("Enter the grid level at W point:\t");
-scanf("%d", &lmin);
-fprintf(fp, "%d,", lmin);
+    fprintf(fp, "W,");
+    printf("Enter the grid level at W point:\t");
+    scanf("%d", &lmin);
+    fprintf(fp, "%d,", lmin);
 }
 printf("\nEnter the no. of levels: ");
 scanf("%d", &ln);
@@ -70,10 +70,10 @@ fprintf(fp, "%d,", cyc);
 /*Solution Initialization__________________________________________________________________________________________________*/
 for (i=0; i<=n; i++)
 {
-for (j=0; j<=m; j++)
-{
-T[i][j]=0;
-}
+    for (j=0; j<=m; j++)
+    {
+        T[i][j]=0;
+    }
 }
 /*_________________________________________________________________________________________________________________________*/
 
@@ -81,83 +81,83 @@ T[i][j]=0;
 clock_t begin = clock();
 while (1)
 {
-BE=0;
-
+    BE=0;
 /*Gauss-Seidel____________________________________________________________________________________________________________*/
-for (i= 1; i<n; i++)
-{
-for (j= 1; j<m; j++)
-{
-T[i][j]= (x2*(T[i+1][j] + T[i-1][j]) + y2*(T[i][j+1] + T[i][j-1])+ 2*x2*y2*((1-6*x2*j*j)*y2*i*i*(1-y2*i*i) + (1-(6*y2*i*i))*x2*j*j*(1-x2*j*j)))/(2*(x2+y2));
-}
-}
+    for (i= 1; i<n; i++)
+    {
+        for (j= 1; j<m; j++)
+        {
+            T[i][j]= (x2*(T[i+1][j] + T[i-1][j]) + y2*(T[i][j+1] + T[i][j-1])+ 2*x2*y2*((1-6*x2*j*j)*y2*i*i*(1-y2*i*i) + (1-(6*y2*i*i))*x2*j*j*(1-x2*j*j)))/(2*(x2+y2));
+        }
+    }
+    
+    fprintf(fp3, "%g,", T[n/2][m/2]);
 
-fprintf(fp3, "%g,", T[n/2][m/2]);
-
-for (i= 1; i<n; i++)
-{
-for (j= 1; j<m; j++)
-{
-res[i][j] =-2*((1-6*x2*j*j)*y2*i*i*(1-y2*i*i) + (1-6*y2*i*i)*x2*j*j*(1-x2*j*j)) - (T[(i+1)][j] + T[(i-1)][j]- 2*T[i][j])/y2 - (T[i][(j+1)] + T[i][(j-1)]- 2*T[i][j])/x2;
-}
-}
+    for (i= 1; i<n; i++)
+    {
+        for (j= 1; j<m; j++)
+        {
+            res[i][j] =-2*((1-6*x2*j*j)*y2*i*i*(1-y2*i*i) + (1-6*y2*i*i)*x2*j*j*(1-x2*j*j)) - (T[(i+1)][j] + T[(i-1)][j]- 2*T[i][j])/y2 - (T[i][(j+1)] + T[i][(j-1)]- 2*T[i][j])/x2;
+        }
+    }
 /*_________________________________________________________________________________________________________________________*/
 
 /*Multigrid________________________________________________________________________________________________________________*/
-iter=1;
-if (sch == 'V')
-{
-while(iter<=cyc)
-{
-k=n/w;
-l=m/w;
-level++;
-rest();
-k=k*w;
-l=l*w;
-while (level>0)
-{
-prolong ();
-}
-add();
-iter++;
-}
-}
+    iter=1;
+    if ((sch == 'V') || (sch=='v'))
+    {
+        while(iter<=cyc)
+        {
+            k=n/w;
+            l=m/w;
+            level++;
+            rest();
+            k=k*w;
+            l=l*w;
+            while (level>0)
+            {
+                prolong ();
+            }
+            add();
+            iter++;
+        }
+    }
 
-else if (sch == 'W')
-{
-while(iter<=cyc)
-{
-k=n/2;
-l=m/2;
-level++;
-rest();
-k=k*2;
-l=l*2;
-while (level>lmin)
-{
-prolong ();
-}
-k=k/2;
-l=l/2;
-rest();
-k=k*2;
-l=l*2;
-while (level>0)
-{
-prolong ();
-}
-add();
-iter++;
-}
-}
+    else if ((sch == 'W') || (sch == 'w'))
+    {
+        while(iter<=cyc)
+        {
+            k=n/2;
+            l=m/2;
+            level++;
+            rest();
+            k=k*2;
+            l=l*2;
+        while (level>lmin)
+        {
+            prolong ();
+        }
+        k=k/2;
+        l=l/2;
+        rest();
+        k=k*2;
+        l=l*2;
+        while (level>0)
+        {
+            prolong ();
+        }
+        add();
+        iter++;
+        }
+    }
 /*_________________________________________________________________________________________________________________________*/
 
-fprintf(fp3, "%g\n", BE);
+    fprintf(fp3, "%g\n", BE);
 
-if (BE<e)
-break;
+    if (BE<e)
+        break;
 }
+
 clock_t end= clock();
 double time=(double)(end-begin)/CLOCKS_PER_SEC;
 
@@ -169,14 +169,14 @@ fprintf(fp, "%d,%g\n", init, time);
 fprintf(fp2, "zone\ni=%d, j=%d\n", n+1, m+1);
 for (i=0; i<=n; i++)
 {
-for (j=0; j<=m; j++)
-{
-Tcalc= x2*y2*i*i*j*j*(1-j*j*x2)*(i*i*y2-1);
-er = Tcalc - T[i][j];
-printf("%g  ", T[i][j]);
-fprintf(fp2, "%f  %f  %g\n", x*j, y*i, T[i][j]);      //Tecplot File
-}
-printf("\n");
+    for (j=0; j<=m; j++)
+    {
+    Tcalc= x2*y2*i*i*j*j*(1-j*j*x2)*(i*i*y2-1);
+    er = Tcalc - T[i][j];
+    printf("%g  ", T[i][j]);
+    fprintf(fp2, "%f  %f  %g\n", x*j, y*i, T[i][j]);      //Tecplot File
+    }
+    printf("\n");
 }
 fclose (fp2);
 fclose (fp);
@@ -187,8 +187,8 @@ printf("\n\n\nRun again with same conditions: [Y]\t[N]\n");
 scanf("%*c%c", &choice);
 if (choice=='Y')
 {
-level++;
-goto x;
+    level++;
+    goto x;
 }
 
 return 0;
@@ -197,70 +197,70 @@ return 0;
 
 void add()
 {
-for (i=1; i<n; i++)
-{
-for (j=1; j<m; j++)
-{
-Tn = T[i][j] + err[i][j];
-T[i][j] = Tn;
-if (fabs(res[i][j])>BE)
-BE = fabs(res[i][j]);
-}
-}
+    for (i=1; i<n; i++)
+    {
+        for (j=1; j<m; j++)
+        {
+            Tn = T[i][j] + err[i][j];
+            T[i][j] = Tn;
+            if (fabs(res[i][j])>BE)
+                BE = fabs(res[i][j]);
+        }
+    }
 }
 
 void rest()
 {
-while (level<=ln)
-{
-    f = power(w ,level);
-    f2= f*f;
-for (a=1; a<init; a++)
-{
-for (i= 1; i<k; i++)
-{
-for (j= 1; j<l; j++)
-{
-res[f*i][f*j] = (res[f*(i-1)][f*j]+res[f*(i+1)][f*j]+res[f*i][f*(j+1)]+res[f*i][f*(j-1)])/4;
-err[f*i][f*j] = (f2*y2*(err[f*(i+1)][f*j]+err[f*(i-1)][f*j]) + f2*x2*(err[f*i][f*(j+1)]+err[f*i][f*(j-1)])-f2*f2*x2*y2*res[f*i][f*j])/(2*f2*(x2+y2));
-}
-}
-}
-for (i= 1; i<n; i++)
-{
-for (j= 1; j<m; j++)
-{
-err[i][j]= (err[(i+1)][j]+err[(i-1)][j] + err[i][(j+1)]+err[i][(j-1)])/4;
-}
-}
-    k = k/w;
-    l=l/w;
-    level++;
-}
-lmax=level;
+    while (level<=ln)
+    {
+        f = power(w ,level);
+        f2= f*f;
+        for (a=1; a<init; a++)
+        {
+            for (i= 1; i<k; i++)
+            {
+                for (j= 1; j<l; j++)
+                {
+                    res[f*i][f*j] = (res[f*(i-1)][f*j]+res[f*(i+1)][f*j]+res[f*i][f*(j+1)]+res[f*i][f*(j-1)])/4;
+                    err[f*i][f*j] = (f2*y2*(err[f*(i+1)][f*j]+err[f*(i-1)][f*j]) + f2*x2*(err[f*i][f*(j+1)]+err[f*i][f*(j-1)])-f2*f2*x2*y2*res[f*i][f*j])/(2*f2*(x2+y2));
+                }
+            }
+        }
+        for (i= 1; i<n; i++)
+        {
+            for (j= 1; j<m; j++)
+            {
+                err[i][j]= (err[(i+1)][j]+err[(i-1)][j] + err[i][(j+1)]+err[i][(j-1)])/4;
+            }
+        }
+        k = k/w;
+        l=l/w;
+        level++;
+    }
+    lmax=level;
 }
 
 int prolong()
 {
     level--;
     f = power(w,level);
-for (i= 1; i<k; i++)
-{
-for (j= 1; j<l; j++)
-{
-err[f*i][f*j]= (4*err[f*i][f*j]+err[f*(i+1)][f*j]+err[f*(i-1)][f*j] + err[f*i][f*(j+1)]+err[f*i][f*(j-1)])/8;
-}
-}
-k=w*k;
-l=w*l;
-return level;
+    for (i= 1; i<k; i++)
+    {
+        for (j= 1; j<l; j++)
+        {
+            err[f*i][f*j]= (4*err[f*i][f*j]+err[f*(i+1)][f*j]+err[f*(i-1)][f*j] + err[f*i][f*(j+1)]+err[f*i][f*(j-1)])/8;
+        }
+    }
+    k=w*k;
+    l=w*l;
+    return level;
 }
 
 int power(int w, int level)
 {
-int i, f=1;
-for (i=1; i<=level; i++)
-f=f*w;
-i++;
-return f;
+    int i, f=1;
+    for (i=1; i<=level; i++)
+        f=f*w;
+    i++;
+    return f;
 }
